e6fce313b5ef9b2fe68712d229d91181e1ba97d9
Vladimir <vovikdoc@gmail.com>
2022-11-19 13:03:36 +0700

    Fix

---
+++ src/TaskSchedulerVsSynchronizationContext/SimpleThreadSynchronizationContext.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskSchedulerVsSynchronizationContext;
+
+internal class SimpleSynchronizationContext : SynchronizationContext
+{
+    public override void Post(SendOrPostCallback d, object? state)
+    {
+        WriteLine($"Post from SimpleSynchronizationContext in {Thread.CurrentThread.ManagedThreadId}");
+        new Thread(() => d.Invoke(state)).Start();
+    }
+
+    public override void Send(SendOrPostCallback d, object? state)
+    {
+        WriteLine($"Send from SimpleSynchronizationContext in {Thread.CurrentThread.ManagedThreadId}");
+        d.Invoke(state);
+    }
+}
